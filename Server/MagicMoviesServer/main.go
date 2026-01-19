01105c54c429a7504ca16a4b0fd5bfbf294979ca
swapno mondol <swapno963@gmail.com>
2026-01-19 15:29:41 +0600

    Project is not running on port 8080 with a get api

--- Server/MagicMoviesServer/main.go
+++ Server/MagicMoviesServer/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println(("Hello go"))
+	router := gin.Default()
+	router.GET("/hello", func(c *gin.Context) {
+		c.String(200, "Hello Go coders")
+	})
+
+	if err := router.Run(":8080"); err != nil {
+		fmt.Println("Failed to start", err)
+	}
 }
