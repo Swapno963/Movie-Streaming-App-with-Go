e78632d07ed6bc276cb9671bd969ff9bae0a308a
swapno mondol <swapno963@gmail.com>
2026-01-21 12:30:47 +0600

    Installed dotenv, loaded env variables on main.go  and mongodb driver

--- Server/MagicMoviesServer/main.go
+++ Server/MagicMoviesServer/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -11,6 +13,11 @@ func main() {
 	router.GET("/hello", func(c *gin.Context) {
 		c.String(200, "Hello Go coders")
 	})
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Println("Warning: unable to find .env file")
+	}
 
 	if err := router.Run(":8080"); err != nil {
 		fmt.Println("Failed to start", err)
