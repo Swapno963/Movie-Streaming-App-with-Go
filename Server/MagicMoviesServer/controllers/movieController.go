ca03eed11cc200288db998acc82665da734edc5d
swapno mondol <swapno963@gmail.com>
2026-01-21 15:06:03 +0600

    added get single movie api ready

--- Server/MagicMoviesServer/controllers/movieController.go
+++ Server/MagicMoviesServer/controllers/movieController.go
@@ -37,3 +37,28 @@ func GetMovies() gin.HandlerFunc {
 
 	}
 }
+
+func GetMovie() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(c, 100*time.Second)
+		defer cancel()
+
+		movieId := c.Param("imdb_id")
+		if movieId == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Move ID is required"})
+			return
+		}
+
+		var movieCollection *mongo.Collection = database.OpenCollection("movies")
+		var movie models.Movie
+
+		err := movieCollection.FindOne(ctx, bson.D{{Key: "imdb_id", Value: movieId}}).Decode(&movie)
+
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Movie not found"})
+			return
+		}
+
+		c.JSON(http.StatusOK, movie)
+	}
+}
